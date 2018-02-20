831e740b87bf5e2fa2ceb5c3eb4e9393e204f385
Lucas Wohlhart <lucas@wohlhart.at>
2018-02-20 01:54:35 +0100

    first IK try

--- IK_debug.py
+++ IK_debug.py
@@ -3,6 +3,57 @@ from time import time
 from mpmath import radians
 import tf
 
+from sympy.matrices import Matrix
+from sympy.matrices.dense import rot_axis1, rot_axis2, rot_axis3
+
+q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+dh_params = {
+    alpha0:     0, a0:      0, d1:  0.75,
+    alpha1: -pi/2, a1:   0.35, d2:     0, q2: q2 - pi/2,
+    alpha2:     0, a2:   1.25, d3:     0, 
+    alpha3: -pi/2, a3: -0.054, d4:   1.5, 
+    alpha4:  pi/2, a4:      0, d5:     0, 
+    alpha5: -pi/2, a5:      0, d6:     0, 
+    alpha6:     0, a6:      0, d7: 0.303, q7: 0 }
+
+
+def dh_transform(alpha, a, theta, d):
+    return Matrix([[           cos(theta),           -sin(theta),           0,             a],
+                   [sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                   [sin(theta)*sin(alpha), cos(theta)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                   [                    0,                     0,           0,             1]])
+
+T0_1 = dh_transform(alpha0, a0, q1, d1).subs(dh_params)
+T1_2 = dh_transform(alpha1, a1, q2, d2).subs(dh_params)
+T2_3 = dh_transform(alpha2, a2, q3, d3).subs(dh_params)
+T3_4 = dh_transform(alpha3, a3, q4, d4).subs(dh_params)
+T4_5 = dh_transform(alpha4, a4, q5, d5).subs(dh_params)
+T5_6 = dh_transform(alpha5, a5, q6, d6).subs(dh_params)
+T6_G = dh_transform(alpha6, a6, q7, d7).subs(dh_params)
+
+T0_2 = simplify(T0_1 * T1_2)
+T0_3 = simplify(T0_2 * T2_3)
+T0_4 = simplify(T0_3 * T3_4)
+T0_5 = simplify(T0_4 * T4_5)
+T0_6 = simplify(T0_5 * T5_6)
+T0_G = simplify(T0_6 * T6_G)
+
+def Rot_X(angle):
+    return rot_axis1(angle).transpose()
+
+def Rot_Y(angle):
+    return rot_axis2(angle).transpose()
+
+def Rot_Z(angle):
+    return rot_axis3(angle).transpose()
+            
+R_corr = simplify(Rot_Z(pi) * Rot_Y(-pi/2))
+
+
 '''
 Format of test case is [ [[EE position],[EE orientation as quaternions]],[WC location],[joint angles]]
 You can generate additional test cases by setting up your kuka project and running `$ roslaunch kuka_arm forward_kinematics.launch`
@@ -71,6 +122,54 @@ def test_code(test_case):
     theta5 = 0
     theta6 = 0
 
+
+
+
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+        [req.poses[x].orientation.x, req.poses[x].orientation.y,
+            req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+    ### Your IK code here
+    # Compensate for rotation discrepancy between DH parameters and Gazebo
+    #
+    #
+    Rrpy = Rot_Z(yaw) * Rot_Y(pitch) * Rot_X(roll) * R_corr
+    
+    # Calculate wrist center position
+    wx, wy, wz = Matrix([px, py, pz]) - (dh_params[d6] + dh_params[d7]) * Rrpy[:,2]
+
+    # Calculate joint angles using Geometric IK method
+    #
+    #
+    ###
+    theta1 = atan2(wy, wx)
+    
+    # Calculate theta2 and theta3 from triangle spanned by joint2, joint3 and wrist-center
+    tri_fwd = sqrt(wx * wx + wy * wy) - dh_params[a1]
+    tri_up = wz - dh_params[d1]
+
+    len_A = sqrt(dh_params[d4] * dh_params[d4] + dh_params[a3] * dh_params[a3])
+    len_B = sqrt(tri_fwd * tri_fwd + tri_up * tri_up)
+    len_C = dh_params[a2]
+
+    theta2 = (pi/2).evalf() - acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C)) - atan2(tri_up, tri_fwd)
+    theta3 = acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) - (pi/2).evalf()
+
+    R0_3 = T0_3[0:3,0:3]
+    R0_3 = R0_3.subs({q1:theta1, q2:theta2, q3: theta3})
+    R3_6 = R0_3.inv("LU") * Rrpy
+
+    # attempt 1
+    theta4 = atan2(R3_6[1,2], R3_6[0,2])
+    theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[1,2] * R3_6[1,2] ), R3_6[2,2])
+    theta6 = atan2(R3_6[2,1], -R3_6[2,0])
+
+    print(simplify(T3_4*T4_5*T5_6)[0:3,0:3])
+    print(theta1, theta2, theta3, theta4, theta5, theta6)
     ## 
     ########################################################################################
     
@@ -84,9 +183,13 @@ def test_code(test_case):
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    #your_wc = [wx,wy,wz] # <--- Load your calculated WC values in this array
+    #your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
+    print(T0_3.evalf(subs={q1:theta1, q2: theta2, q3:-0.36, q4:theta4, q4: theta5, q6: theta6}))
+    print(T0_3.evalf(subs={q1:theta1, q2: theta2, q3:-0.36, q4:theta4, q4: theta5, q6: theta6})[3,:])
+    your_wc = T0_3.evalf(subs={q1:theta1, q2: theta2, q3:-0.36, q4:theta4, q4: theta5, q6: theta6})[3,:]
+    your_ee = T0_6.evalf(subs={q1:theta1, q2: theta2, q3:-0.36, q4:theta4, q4: theta5, q6: theta6})[3,:]
 
     ## Error analysis
     print ("\nTotal run time to calculate joint angles from pose is %04.4f seconds" % (time()-start_time))
--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -17,7 +17,55 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
+from sympy.matrices import Matrix
+from sympy.matrices.dense import rot_axis1, rot_axis2, rot_axis3
 
+q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+dh_params = {
+    alpha0:     0, a0:      0, d1:  0.75,
+    alpha1: -pi/2, a1:   0.35, d2:     0, q2: q2 - pi/2,
+    alpha2:     0, a2:   1.25, d3:     0, 
+    alpha3: -pi/2, a3: -0.054, d4:   1.5, 
+    alpha4:  pi/2, a4:      0, d5:     0, 
+    alpha5: -pi/2, a5:      0, d6:     0, 
+    alpha6:     0, a6:      0, d7: 0.303, q7: 0 }
+
+
+def dh_transform(alpha, a, theta, d):
+    return Matrix([[           cos(theta),           -sin(theta),           0,             a],
+                   [sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                   [sin(theta)*sin(alpha), cos(theta)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                   [                    0,                     0,           0,             1]])
+
+T0_1 = dh_transform(alpha0, a0, q1, d1).subs(dh_params)
+T1_2 = dh_transform(alpha1, a1, q2, d2).subs(dh_params)
+T2_3 = dh_transform(alpha2, a2, q3, d3).subs(dh_params)
+T3_4 = dh_transform(alpha3, a3, q4, d4).subs(dh_params)
+T4_5 = dh_transform(alpha4, a4, q5, d5).subs(dh_params)
+T5_6 = dh_transform(alpha5, a5, q6, d6).subs(dh_params)
+T6_G = dh_transform(alpha6, a6, q7, d7).subs(dh_params)
+
+T0_2 = simplify(T0_1 * T1_2)
+T0_3 = simplify(T0_2 * T2_3)
+T0_4 = simplify(T0_3 * T3_4)
+T0_5 = simplify(T0_4 * T4_5)
+T0_6 = simplify(T0_5 * T5_6)
+T0_G = simplify(T0_6 * T6_G)
+
+def Rot_X(angle):
+    return rot_axis1(angle).transpose()
+
+def Rot_Y(angle):
+    return rot_axis2(angle).transpose()
+
+def Rot_Z(angle):
+    return rot_axis3(angle).transpose()
+            
+R_corr = simplify(Rot_Z(pi) * Rot_Y(-pi/2))
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -28,20 +76,20 @@ def handle_calculate_IK(req):
 
         ### Your FK code here
         # Create symbols
-	#
-	#
-	# Create Modified DH parameters
-	#
-	#
-	# Define Modified DH Transformation matrix
-	#
-	#
-	# Create individual transformation matrices
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-	#
-	#
+        #
+        #
+        # Create Modified DH parameters
+        #
+        #
+        # Define Modified DH Transformation matrix
+        #
+        #
+        # Create individual transformation matrices
+        #
+        #
+        # Extract rotation matrices from the transformation matrices
+        #
+        #
         ###
 
         # Initialize service response
@@ -50,9 +98,9 @@ def handle_calculate_IK(req):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
 
-	    # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
+            # Extract end-effector position and orientation from request
+            # px,py,pz = end-effector position
+            # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
@@ -62,18 +110,44 @@ def handle_calculate_IK(req):
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
 
             ### Your IK code here
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    #
-	    #
-	    # Calculate joint angles using Geometric IK method
-	    #
-	    #
+            # Compensate for rotation discrepancy between DH parameters and Gazebo
+            #
+            #
+            Rrpy = Rot_Z(yaw) * Rot_Y(pitch) * Rot_X(roll) * R_corr
+            
+            # Calculate wrist center position
+            wx, wy, wz = Matrix([px, py, pz]) - (dh_params.d6 + dh_params.d7) * Rrpy[:,2]
+
+            # Calculate joint angles using Geometric IK method
+            #
+            #
             ###
+            theta1 = atan2(wy, wx)
+            
+            # Calculate theta2 and theta3 from triangle spanned by joint2, joint3 and wrist-center
+            tri_fwd = sqrt(wx * wx + wy * wy) - dh_params.a1
+            tri_up = wz - dh_params.d1
+
+            len_A = sqrt(dh_params.d4 * dh_params.d4 + dh_params.a3 * dh_params.a3)
+            len_B = sqrt(tri_fwd * tri_fwd + tri_up * tri_up)
+            len_C = dh_params.a2
+
+            theta2 = pi/2 - acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C)) - atan2(tri_up, tri_fwd)
+            theta3 = acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) - pi/2
+
+            R0_3 = T0_3[0:3,0:3]
+            R0_3 = R0_3.subs({q1:theta1, q2:theta2, q3: theta3})
+            R3_6 = R0_3.inv("LU") * Rrpy
+
+            # attempt 1
+            theta4 = atan2(R3_6[1,2], R3_6[0,2])
+            theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[1,2] * R3_6[1,2] ), R3_6[2,2])
+            theta6 = atan2(R3_6[2,1], -R3_6[2,0])
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+            joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+            joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
