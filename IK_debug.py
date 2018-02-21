287ab10d63dc2c8810198e244360658e0f807fe6
Lucas Wohlhart <lucas@wohlhart.at>
2018-02-21 18:26:07 +0100

    inverse kinematics; add and select best solution from alternatives

--- IK_debug.py
+++ IK_debug.py
@@ -1,7 +1,8 @@
-from sympy import *
+from sympy import sqrt, atan2, acos, symbols, pi, cos, sin, simplify, sympify
 from time import time
 from mpmath import radians
 import tf
+import numpy as np
 
 from sympy.matrices import Matrix
 from sympy.matrices.dense import rot_axis1, rot_axis2, rot_axis3
@@ -14,10 +15,10 @@ alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 dh_params = {
     alpha0:     0, a0:      0, d1:  0.75,
     alpha1: -pi/2, a1:   0.35, d2:     0, q2: q2 - pi/2,
-    alpha2:     0, a2:   1.25, d3:     0, 
-    alpha3: -pi/2, a3: -0.054, d4:   1.5, 
-    alpha4:  pi/2, a4:      0, d5:     0, 
-    alpha5: -pi/2, a5:      0, d6:     0, 
+    alpha2:     0, a2:   1.25, d3:     0,
+    alpha3: -pi/2, a3: -0.054, d4:   1.5,
+    alpha4:  pi/2, a4:      0, d5:     0,
+    alpha5: -pi/2, a5:      0, d6:     0,
     alpha6:     0, a6:      0, d7: 0.303, q7: 0 }
 
 
@@ -35,12 +36,20 @@ T4_5 = dh_transform(alpha4, a4, q5, d5).subs(dh_params)
 T5_6 = dh_transform(alpha5, a5, q6, d6).subs(dh_params)
 T6_G = dh_transform(alpha6, a6, q7, d7).subs(dh_params)
 
-T0_2 = simplify(T0_1 * T1_2)
-T0_3 = simplify(T0_2 * T2_3)
-T0_4 = simplify(T0_3 * T3_4)
-T0_5 = simplify(T0_4 * T4_5)
-T0_6 = simplify(T0_5 * T5_6)
-T0_G = simplify(T0_6 * T6_G)
+# T0_2 = simplify(T0_1 * T1_2)
+# T0_3 = simplify(T0_2 * T2_3)
+# T0_4 = simplify(T0_3 * T3_4)
+# T0_5 = simplify(T0_4 * T4_5)
+# T0_6 = simplify(T0_5 * T5_6)
+# T0_G = simplify(T0_6 * T6_G)
+T0_2 = T0_1 * T1_2
+T0_3 = T0_2 * T2_3
+T0_4 = T0_3 * T3_4
+T0_5 = T0_4 * T4_5
+T0_6 = T0_5 * T5_6
+T0_G = T0_6 * T6_G
+
+R0_3 = T0_1[0:3,0:3]*T1_2[0:3,0:3]*T2_3[0:3,0:3]
 
 def Rot_X(angle):
     return rot_axis1(angle).transpose()
@@ -50,7 +59,7 @@ def Rot_Y(angle):
 
 def Rot_Z(angle):
     return rot_axis3(angle).transpose()
-            
+
 R_corr = simplify(Rot_Z(pi) * Rot_Y(-pi/2))
 
 
@@ -76,6 +85,77 @@ test_cases = {1:[[[2.16135,-1.42635,1.55109],
               4:[],
               5:[]}
 
+class JointsOption:
+    LIMITS = [[-3.23, 3.23],[-0.79, 1.48], [-3.67, 1.13], [-6.11, 6.11], [-2.18, 2.18], [-6.11, 6.11]]
+    def __init__(self):
+        self.theta1 = 0
+        self.theta2 = 0
+        self.theta3 = 0
+        self.theta4 = 0
+        self.theta5 = 0
+        self.theta6 = 0
+        self.wx_tilde = 0
+        self.wz_tilde = 0
+        self.valid = True
+
+    def clone(self):
+        clone = JointsOption()
+        clone.theta1 = self.theta1
+        clone.theta2 = self.theta2
+        clone.theta3 = self.theta3
+        clone.theta4 = self.theta4
+        clone.theta5 = self.theta5
+        clone.theta6 = self.theta6
+        clone.wx_tilde = self.wx_tilde
+        clone.wz_tilde = self.wz_tilde
+        clone.valid = self.valid
+        return clone
+
+    def thetas(self):
+        return (self.theta1, self.theta2, self.theta3, self.theta4, self.theta5, self.theta6)
+
+    def wc_error(self, wc):
+        fk_wc = T0_4.evalf(subs={q1:self.theta1, q2:self.theta2, q3:self.theta3, q4:self.theta4, q5:self.theta5, q6:self.theta6})[:3,3]
+        wc_x_e = abs(fk_wc[0]-wc[0])
+        wc_y_e = abs(fk_wc[1]-wc[1])
+        wc_z_e = abs(fk_wc[2]-wc[2])
+        return sqrt(wc_x_e**2 + wc_y_e**2 + wc_z_e**2)
+
+    def ee_error(self, ee):
+        fk_ee = T0_G.evalf(subs={q1:self.theta1, q2:self.theta2, q3:self.theta3, q4:self.theta4, q5:self.theta5, q6:self.theta6})[:3,3]
+        ee_x_e = abs(fk_ee[0]-ee[0])
+        ee_y_e = abs(fk_ee[1]-ee[1])
+        ee_z_e = abs(fk_ee[2]-ee[2])
+        return sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2)
+
+    def orientation_error(self, orientation):        
+        corr = Matrix.eye(4)
+        corr[:3,:3] = R_corr
+        rot_mat = T0_G.evalf(subs={q1:self.theta1, q2:self.theta2, q3:self.theta3, q4:self.theta4, q5:self.theta5, q6:self.theta6})
+        rot_mat = rot_mat * corr
+        fk_orientation = tf.transformations.quaternion_from_matrix(rot_mat.tolist())
+        err = np.linalg.norm(fk_orientation - np.array([orientation.x, orientation.y, orientation.z, orientation.w]))
+        return err
+
+    def fk_error(self, wc, ee, orientation):
+
+        return self.wc_error(wc) + self.ee_error(ee) + self.orientation_error(orientation)
+
+
+    def check_valid(self):
+        if not self.valid:
+            return False
+        thetas = self.thetas()
+        for i in range(len(thetas)):
+            if not sympify(thetas[i]).is_real:
+                return False
+            if not (JointsOption.LIMITS[i][0] <= thetas[i] and thetas[i] <= JointsOption.LIMITS[i][1]):
+                # print ("theta{}".format(i), thetas[i].evalf(), "not in limits", JointsOption.LIMITS[i])
+                return False
+        return True
+
+    def debug_print(self):
+        print([t.evalf() for t in self.thetas()])
 
 def test_code(test_case):
     ## Set up code
@@ -109,21 +189,11 @@ def test_code(test_case):
 
     req = Pose(comb)
     start_time = time()
-    
+
     ########################################################################################
-    ## 
+    ##
 
     ## Insert IK code here!
-    
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
-    theta4 = 0
-    theta5 = 0
-    theta6 = 0
-
-
-
 
     px = req.poses[x].position.x
     py = req.poses[x].position.y
@@ -138,7 +208,7 @@ def test_code(test_case):
     #
     #
     Rrpy = Rot_Z(yaw) * Rot_Y(pitch) * Rot_X(roll) * R_corr
-    
+
     # Calculate wrist center position
     wx, wy, wz = Matrix([px, py, pz]) - (dh_params[d6] + dh_params[d7]) * Rrpy[:,2]
 
@@ -146,32 +216,85 @@ def test_code(test_case):
     #
     #
     ###
-    theta1 = atan2(wy, wx)
-    
+    joints = []
+    joints.append(JointsOption())
+    joints.append(JointsOption())
+    joints[0].theta1 = atan2(wy, wx)
+    joints[1].theta1 = atan2(-wy,-wx) # 2nd_alternative    
+
     # Calculate theta2 and theta3 from triangle spanned by joint2, joint3 and wrist-center
-    tri_fwd = sqrt(wx * wx + wy * wy) - dh_params[a1]
-    tri_up = wz - dh_params[d1]
+    joints[0].wx_tilde = sqrt(wx * wx + wy * wy) - dh_params[a1] # 1st_alternative
+    joints[0].wz_tilde = wz - dh_params[d1]
+    joints[1].wx_tilde = -sqrt(wx * wx + wy * wy) - dh_params[a1] # 2nd_alternative
+    joints[1].wz_tilde = wz - dh_params[d1]
 
-    len_A = sqrt(dh_params[d4] * dh_params[d4] + dh_params[a3] * dh_params[a3])
-    len_B = sqrt(tri_fwd * tri_fwd + tri_up * tri_up)
-    len_C = dh_params[a2]
+    joints = [j for j in joints if j.check_valid()]
 
-    theta2 = atan2(tri_fwd, tri_up) - acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C))
-    theta3 = pi/2 - acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) + atan2(dh_params[a3], dh_params[d4])
+    joints_alt = []
+    for j in joints:
 
-    R0_3 = T0_1[0:3,0:3]*T1_2[0:3,0:3]*T2_3[0:3,0:3]
-    R0_3 = R0_3.evalf(subs={q1:theta1, q2:theta2, q3: theta3})
-    R3_6 = R0_3.inv("LU") * Rrpy
+        len_A = sqrt(dh_params[d4] * dh_params[d4] + dh_params[a3] * dh_params[a3])
+        len_B = sqrt(j.wx_tilde * j.wx_tilde + j.wz_tilde * j.wz_tilde)
+        len_C = dh_params[a2]
+        if len_B > len_A + len_C:
+            j.valid = False
+            continue
 
-    # attempt 1
-    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-    theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2] ), R3_6[2,2])
-    #theta5 = (acos(R3_6[1,2])) # unstable but more accurate alternative
-    #print(theta5)
-    theta6 = atan2(-R3_6[1,1], R3_6[1,0])    
-    ## 
+        j_alt = j.clone()
+
+        j.theta2 = atan2(j.wx_tilde, j.wz_tilde) - acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C)) # 1nd_alternative
+        j.theta3 = pi/2 - acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) + atan2(dh_params[a3], dh_params[d4]) # 1st_alternative
+
+        j_alt.theta2 = atan2(j.wx_tilde, j.wz_tilde) + acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C)) # 2nd_alternative
+        j_alt.theta3 = -3*pi/2 + acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) - atan2(dh_params[a3], dh_params[d4]) # 2nd_alternative
+        joints_alt.append(j_alt)
+
+    joints = [j for j in (joints + joints_alt) if j.check_valid()]
+    joints_alt = []
+    for j in joints:        
+        R0_3_current = R0_3.evalf(subs={q1:j.theta1, q2:j.theta2, q3: j.theta3})
+        R3_6 = R0_3_current.inv("LU") * Rrpy
+
+        # R3_6(q4, q5, q6) structure:
+        #[[-sin(q4)*sin(q6) + cos(q4)*cos(q5)*cos(q6), -sin(q4)*cos(q6) - sin(q6)*cos(q4)*cos(q5), -sin(q5)*cos(q4)],
+        # [                           sin(q5)*cos(q6),                           -sin(q5)*sin(q6),          cos(q5)],
+        # [-sin(q4)*cos(q5)*cos(q6) - sin(q6)*cos(q4),  sin(q4)*sin(q6)*cos(q5) - cos(q4)*cos(q6),  sin(q4)*sin(q5)]]
+        
+        j.theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+        j.theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2] ), R3_6[2,2])
+        j.theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+
+        j_alt = j.clone()
+        j_alt.theta5 = (acos(R3_6[1,2])) # ambiguous but numerically more accurate alternative
+        joints_alt.append(j_alt)
+
+        # j_alt = j.clone()
+        # j_alt.theta6 = atan2(R3_6[1,1], -R3_6[1,0])
+        # joints_alt.append(j_alt)
+
+        # j_alt = j.clone()        
+        # j_alt.theta4 = atan2(-R3_6[2,2], R3_6[0,2])
+        # joints_alt.append(j_alt)
+
+        # j_alt = j.clone()        
+        # j_alt.theta4 = atan2(-R3_6[2,2], R3_6[0,2])
+        # j_alt.theta6 = atan2(R3_6[1,1], -R3_6[1,0])
+        # joints_alt.append(j_alt)        
+
+
+    joints = [j for j in (joints + joints_alt) if j.check_valid()]
+    # for j in joints:
+    #     j.debug_print()
+    #     print(j.fk_error([wx, wy, wz], [px, py, pz], req.poses[x].orientation))
+
+    # print(len(joints))
+    joints = sorted(joints, key = lambda j: j.fk_error([wx, wy, wz], [px, py, pz], req.poses[x].orientation))
+    (theta1, theta2, theta3, theta4, theta5, theta6) = joints[0].thetas()
+    # print(test_case)
+    # print(theta1.evalf(), theta2.evalf(), theta3.evalf(), theta4.evalf(), theta5.evalf(), theta6.evalf())
+    ##
     ########################################################################################
-    
+
     ########################################################################################
     ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
@@ -185,9 +308,9 @@ def test_code(test_case):
     #your_wc = [wx,wy,wz] # <--- Load your calculated WC values in this array
     #your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
-        
+
     your_wc = T0_4.evalf(subs={q1:theta1, q2: theta2, q3:theta3, q4:theta4, q5: theta5, q6: theta6})[:3,3]
-    your_ee = T0_G.evalf(subs={q1:theta1, q2: theta2, q3:theta3, q4:theta4, q5: theta5, q6: theta6})[:3,3]    
+    your_ee = T0_G.evalf(subs={q1:theta1, q2: theta2, q3:theta3, q4:theta4, q5: theta5, q6: theta6})[:3,3]
 
     ## Error analysis
     print ("\nTotal run time to calculate joint angles from pose is %04.4f seconds" % (time()-start_time))
@@ -239,6 +362,7 @@ if __name__ == "__main__":
     # Change test case number for different scenarios
     test_case_number = 1
 
-    test_code(test_cases[test_case_number])
+    #test_code(test_cases[test_case_number])
+    test_code(test_cases[1])
     test_code(test_cases[2])
     test_code(test_cases[3])
--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -16,7 +16,8 @@ from kuka_arm.srv import *
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
 from mpmath import *
-from sympy import *
+import numpy as np
+from sympy import sqrt, atan2, acos, symbols, pi, cos, sin, simplify, sympify
 from sympy.matrices import Matrix
 from sympy.matrices.dense import rot_axis1, rot_axis2, rot_axis3
 
@@ -49,12 +50,20 @@ T4_5 = dh_transform(alpha4, a4, q5, d5).subs(dh_params)
 T5_6 = dh_transform(alpha5, a5, q6, d6).subs(dh_params)
 T6_G = dh_transform(alpha6, a6, q7, d7).subs(dh_params)
 
-T0_2 = simplify(T0_1 * T1_2)
-T0_3 = simplify(T0_2 * T2_3)
-T0_4 = simplify(T0_3 * T3_4)
-T0_5 = simplify(T0_4 * T4_5)
-T0_6 = simplify(T0_5 * T5_6)
-T0_G = simplify(T0_6 * T6_G)
+# T0_2 = simplify(T0_1 * T1_2)
+# T0_3 = simplify(T0_2 * T2_3)
+# T0_4 = simplify(T0_3 * T3_4)
+# T0_5 = simplify(T0_4 * T4_5)
+# T0_6 = simplify(T0_5 * T5_6)
+# T0_G = simplify(T0_6 * T6_G)
+T0_2 = T0_1 * T1_2
+T0_3 = T0_2 * T2_3
+T0_4 = T0_3 * T3_4
+T0_5 = T0_4 * T4_5
+T0_6 = T0_5 * T5_6
+T0_G = T0_6 * T6_G
+
+R0_3 = T0_1[0:3,0:3]*T1_2[0:3,0:3]*T2_3[0:3,0:3]
 
 def Rot_X(angle):
     return rot_axis1(angle).transpose()
@@ -66,6 +75,78 @@ def Rot_Z(angle):
     return rot_axis3(angle).transpose()
             
 R_corr = simplify(Rot_Z(pi) * Rot_Y(-pi/2))
+
+class JointsOption:
+    LIMITS = [[-3.23, 3.23],[-0.79, 1.48], [-3.67, 1.13], [-6.11, 6.11], [-2.18, 2.18], [-6.11, 6.11]]
+    def __init__(self):
+        self.theta1 = 0
+        self.theta2 = 0
+        self.theta3 = 0
+        self.theta4 = 0
+        self.theta5 = 0
+        self.theta6 = 0
+        self.wx_tilde = 0
+        self.wz_tilde = 0
+        self.valid = True
+
+    def clone(self):
+        clone = JointsOption()
+        clone.theta1 = self.theta1
+        clone.theta2 = self.theta2
+        clone.theta3 = self.theta3
+        clone.theta4 = self.theta4
+        clone.theta5 = self.theta5
+        clone.theta6 = self.theta6
+        clone.wx_tilde = self.wx_tilde
+        clone.wz_tilde = self.wz_tilde
+        clone.valid = self.valid
+        return clone
+
+    def thetas(self):
+        return (self.theta1, self.theta2, self.theta3, self.theta4, self.theta5, self.theta6)
+
+    def wc_error(self, wc):
+        fk_wc = T0_4.evalf(subs={q1:self.theta1, q2:self.theta2, q3:self.theta3, q4:self.theta4, q5:self.theta5, q6:self.theta6})[:3,3]
+        wc_x_e = abs(fk_wc[0]-wc[0])
+        wc_y_e = abs(fk_wc[1]-wc[1])
+        wc_z_e = abs(fk_wc[2]-wc[2])
+        return sqrt(wc_x_e**2 + wc_y_e**2 + wc_z_e**2)
+
+    def ee_error(self, ee):
+        fk_ee = T0_G.evalf(subs={q1:self.theta1, q2:self.theta2, q3:self.theta3, q4:self.theta4, q5:self.theta5, q6:self.theta6})[:3,3]
+        ee_x_e = abs(fk_ee[0]-ee[0])
+        ee_y_e = abs(fk_ee[1]-ee[1])
+        ee_z_e = abs(fk_ee[2]-ee[2])
+        return sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2)
+
+    def orientation_error(self, orientation):        
+        corr = Matrix.eye(4)
+        corr[:3,:3] = R_corr
+        rot_mat = T0_G.evalf(subs={q1:self.theta1, q2:self.theta2, q3:self.theta3, q4:self.theta4, q5:self.theta5, q6:self.theta6})
+        rot_mat = rot_mat * corr
+        fk_orientation = tf.transformations.quaternion_from_matrix(rot_mat.tolist())
+        err = np.linalg.norm(fk_orientation - np.array([orientation.x, orientation.y, orientation.z, orientation.w]))
+        return err
+
+    def fk_error(self, wc, ee, orientation):
+
+        return self.wc_error(wc) + self.ee_error(ee) + self.orientation_error(orientation)
+
+
+    def check_valid(self):
+        if not self.valid:
+            return False
+        thetas = self.thetas()
+        for i in range(len(thetas)):
+            if not sympify(thetas[i]).is_real:
+                return False
+            if not (JointsOption.LIMITS[i][0] <= thetas[i] and thetas[i] <= JointsOption.LIMITS[i][1]):
+                # print ("theta{}".format(i), thetas[i].evalf(), "not in limits", JointsOption.LIMITS[i])
+                return False
+        return True
+
+    def debug_print(self):
+        print([t.evalf() for t in self.thetas()])
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -116,33 +197,86 @@ def handle_calculate_IK(req):
             Rrpy = Rot_Z(yaw) * Rot_Y(pitch) * Rot_X(roll) * R_corr
             
             # Calculate wrist center position
-            wx, wy, wz = Matrix([px, py, pz]) - (dh_params.d6 + dh_params.d7) * Rrpy[:,2]
+            wx, wy, wz = Matrix([px, py, pz]) - (dh_params[d6] + dh_params[d7]) * Rrpy[:,2]
 
             # Calculate joint angles using Geometric IK method
             #
             #
             ###
-            theta1 = atan2(wy, wx)
-            
+            joints = []
+            joints.append(JointsOption())
+            joints.append(JointsOption())
+            joints[0].theta1 = atan2(wy, wx)
+            joints[1].theta1 = atan2(-wy,-wx) # 2nd_alternative    
+
             # Calculate theta2 and theta3 from triangle spanned by joint2, joint3 and wrist-center
-            tri_fwd = sqrt(wx * wx + wy * wy) - dh_params.a1
-            tri_up = wz - dh_params.d1
+            joints[0].wx_tilde = sqrt(wx * wx + wy * wy) - dh_params[a1] # 1st_alternative
+            joints[0].wz_tilde = wz - dh_params[d1]
+            joints[1].wx_tilde = -sqrt(wx * wx + wy * wy) - dh_params[a1] # 2nd_alternative
+            joints[1].wz_tilde = wz - dh_params[d1]
 
-            len_A = sqrt(dh_params.d4 * dh_params.d4 + dh_params.a3 * dh_params.a3)
-            len_B = sqrt(tri_fwd * tri_fwd + tri_up * tri_up)
-            len_C = dh_params.a2
+            joints = [j for j in joints if j.check_valid()]
 
-            theta2 = pi/2 - acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C)) - atan2(tri_up, tri_fwd)
-            theta3 = acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) - pi/2
+            joints_alt = []
+            for j in joints:
 
-            R0_3 = T0_3[0:3,0:3]
-            R0_3 = R0_3.subs({q1:theta1, q2:theta2, q3: theta3})
-            R3_6 = R0_3.inv("LU") * Rrpy
+                len_A = sqrt(dh_params[d4] * dh_params[d4] + dh_params[a3] * dh_params[a3])
+                len_B = sqrt(j.wx_tilde * j.wx_tilde + j.wz_tilde * j.wz_tilde)
+                len_C = dh_params[a2]
+                if len_B > len_A + len_C:
+                    j.valid = False
+                    continue
 
-            # attempt 1
-            theta4 = atan2(R3_6[1,2], R3_6[0,2])
-            theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[1,2] * R3_6[1,2] ), R3_6[2,2])
-            theta6 = atan2(R3_6[2,1], -R3_6[2,0])
+                j_alt = j.clone()
+
+                j.theta2 = atan2(j.wx_tilde, j.wz_tilde) - acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C)) # 1nd_alternative
+                j.theta3 = pi/2 - acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) + atan2(dh_params[a3], dh_params[d4]) # 1st_alternative
+
+                j_alt.theta2 = atan2(j.wx_tilde, j.wz_tilde) + acos((len_B*len_B + len_C*len_C - len_A*len_A)/(2*len_B*len_C)) # 2nd_alternative
+                j_alt.theta3 = -3*pi/2 + acos((len_A*len_A + len_C*len_C - len_B*len_B)/(2*len_A*len_C)) - atan2(dh_params[a3], dh_params[d4]) # 2nd_alternative
+                joints_alt.append(j_alt)
+
+            joints = [j for j in (joints + joints_alt) if j.check_valid()]
+            joints_alt = []
+            for j in joints:        
+                R0_3_current = R0_3.evalf(subs={q1:j.theta1, q2:j.theta2, q3: j.theta3})
+                R3_6 = R0_3_current.inv("LU") * Rrpy
+
+                # R3_6(q4, q5, q6) structure:
+                #[[-sin(q4)*sin(q6) + cos(q4)*cos(q5)*cos(q6), -sin(q4)*cos(q6) - sin(q6)*cos(q4)*cos(q5), -sin(q5)*cos(q4)],
+                # [                           sin(q5)*cos(q6),                           -sin(q5)*sin(q6),          cos(q5)],
+                # [-sin(q4)*cos(q5)*cos(q6) - sin(q6)*cos(q4),  sin(q4)*sin(q6)*cos(q5) - cos(q4)*cos(q6),  sin(q4)*sin(q5)]]
+                
+                j.theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+                j.theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2] ), R3_6[2,2])
+                j.theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+
+                j_alt = j.clone()
+                j_alt.theta5 = (acos(R3_6[1,2])) # ambiguous but numerically more accurate alternative
+                joints_alt.append(j_alt)
+
+                # j_alt = j.clone()
+                # j_alt.theta6 = atan2(R3_6[1,1], -R3_6[1,0])
+                # joints_alt.append(j_alt)
+
+                # j_alt = j.clone()        
+                # j_alt.theta4 = atan2(-R3_6[2,2], R3_6[0,2])
+                # joints_alt.append(j_alt)
+
+                # j_alt = j.clone()        
+                # j_alt.theta4 = atan2(-R3_6[2,2], R3_6[0,2])
+                # j_alt.theta6 = atan2(R3_6[1,1], -R3_6[1,0])
+                # joints_alt.append(j_alt)        
+
+
+            joints = [j for j in (joints + joints_alt) if j.check_valid()]
+            # for j in joints:
+            #     j.debug_print()
+            #     print(j.fk_error([wx, wy, wz], [px, py, pz], req.poses[x].orientation))
+
+            # print(len(joints))
+            joints = sorted(joints, key = lambda j: j.fk_error([wx, wy, wz], [px, py, pz], req.poses[x].orientation))
+            (theta1, theta2, theta3, theta4, theta5, theta6) = joints[0].thetas()
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
